06acff88a10bb4f61f3cf16ae4e84b3f5c639bfb
Chewan231 <42933135+Chewan231@users.noreply.github.com>
2018-12-26 13:15:34 +0300

    Added Stock Project Source Code

---
+++ Stock/Stock/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //TO-DO checj Login username & Password
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-KC510KN\SERVER;Initial Catalog=Stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT *
+                FROM [Stock].[dbo].[Login] where UserName = '" + textBox1.Text + "' and Password = '" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("invalid Username & Password !","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                button2_Click(sender, e);
+            }
+            
+        }
+    }
+}
